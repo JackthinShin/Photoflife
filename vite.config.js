9b3d81b962a620185a3102e14f701722b3e05d99
Jackthin <jackthin@foxmail.com>
2026-03-12 17:12:45 +0800

    New

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // 关键配置：为了在 GitHub Pages 上正确加载资源
-})
